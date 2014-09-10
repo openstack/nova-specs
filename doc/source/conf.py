3585a11e68d5b79b7a0428ce035a11b56215150e
Doug Hellmann <doug@doughellmann.com>
2014-09-10 14:50:05 -0400

    Remove templates from toctrees
    We don't want the templates to show up in the RSS feed, so exclude them
    from being converted to HTML and listed in the toctree.
    
    Change-Id: I1d50944fc37f27b5fc67316270cb281e3635b762

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -69,7 +69,11 @@ copyright = u'2014, OpenStack Nova Team'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = [
+    '_build',
+    'specs/juno-template.rst',
+    'specs/kilo-template.rst',
+]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
